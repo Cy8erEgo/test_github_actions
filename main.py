b3cf1c76ec87b26e8703f4ca657f52c0dbbe2e5f
Cyberego <cy8erego@yandex.ru>
2020-06-22 02:50:40 +0700

    Added get_my_ip() function

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import requests as req
+from pprint import pprint
+
+def get_my_ip():
+    url = "http://ip-api.com/json/"
+    JSON = req.get(url).json()
+
+    return JSON
+
 def doubler(n):
     return n * 2
 
@@ -6,3 +15,5 @@ if __name__ == "__main__":
     print(doubler(3))
     print(doubler(5))
     print(doubler(100))
+
+    pprint(get_my_ip())
